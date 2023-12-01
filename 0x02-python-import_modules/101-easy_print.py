6ae77ac04d06dc450b1d8a4140177bc7445826dc
Iamyussef <youssefidomar123@gmail.com>
2023-12-01 13:18:47 -0800

    101-easy_print.py

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-import easy_print
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
